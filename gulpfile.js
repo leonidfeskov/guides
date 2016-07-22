09d52cb9613162d14e53ce1af5d3589df6d293fb
Leonid Feskov <lfeskov@Leonids-iMac.local>
2016-07-22 17:07:22 +0300

    add btn

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var autoprefixer = require('autoprefixer');
 var concat = require('gulp-concat');
 
 var pathSource = 'source';
+var pathComponents = 'source/components';
 var pathBuild = 'htdocs';
 
 // SERVER AND SYNC ==================================
@@ -24,20 +25,31 @@ gulp.task('browserSync', function(){
 });
 
 // CLEAN ALL ========================================
-gulp.task('clean', function () {
+gulp.task('clean', function(){
 	return gulp.src([pathBuild], {read: false})
 		.pipe(clean());
 });
 
-
 // HTML =============================================
-gulp.task('pug', function(){
+gulp.task('clean-pug-mixins', function(){
+	return gulp.src(pathComponents + '/_mixins.pug', {read: false})
+		.pipe(clean());
+});
+
+gulp.task('concat-pug-mixins', ['clean-pug-mixins'], function(){
+	return gulp.src(pathComponents + '/**/_*.pug')
+		.pipe(concat('_mixins.pug'))
+		.pipe(gulp.dest(pathComponents));
+});
+
+gulp.task('pug', ['concat-pug-mixins'], function(){
 	return gulp.src(pathSource + '/tpl/**/*.pug')
 		.pipe(plumber())
 		.pipe(pug({
 			pretty: true
 		}))
-		.pipe(gulp.dest(pathBuild + '/html'));
+		.pipe(gulp.dest(pathBuild + '/html'))
+		.pipe(browserSync.stream());
 });
 // END HTML =========================================
 
@@ -48,16 +60,31 @@ var postProcessors = [
 ];
 
 gulp.task('sass', function(){
-	return gulp.src(pathSource + '/f/sass/**/*.sass')
+	return gulp.src([pathComponents + '/_variables.sass',
+									pathComponents + '/_mixins.sass',
+									pathComponents + '/**/*.sass'])
+		.pipe(concat('components.sass'))
+		.pipe(plumber())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(postcss(postProcessors))
-		.pipe(gulp.dest(pathBuild + '/f/css'));
+		.pipe(gulp.dest(pathBuild + '/f/css'))
+		.pipe(browserSync.stream());
+});
+
+// main.css
+gulp.task('main-css', function(){
+	return gulp.src(pathSource + '/f/sass/**/*.sass')
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(postProcessors))
+		.pipe(gulp.dest(pathBuild + '/f/css'))
+		.pipe(browserSync.stream());
 });
 // END CSS ==========================================
 
 // SCRIPTS ==========================================
 gulp.task('scripts', function() {
-	gulp.src(pathSource + '/f/js/**/*.js')
+	return gulp.src(pathSource + '/f/js/**/*.js')
 		.pipe(gulp.dest(pathBuild + '/f/js'));
 });
 // END SCRIPTS ======================================
@@ -71,17 +98,19 @@ gulp.task('img', function(){
 
 // watch
 gulp.task('watch', function() {
-	gulp.watch(pathSource + '/templates/**/*.pug', ['pug']);
-	gulp.watch(pathSource + '/f/sass/**/*.sass', ['sass']);
-	gulp.watch(pathSource + '/f/i/**/*.*', ['img']);
-	gulp.watch(pathSource + '/f/js/**/*.js', ['scripts']);
+	gulp.watch(pathComponents + '/**/*.pug', ['concat-pug-mixins', 'pug']);
+	gulp.watch(pathSource + '/tpl/**/*.pug', ['pug']);
+	gulp.watch(pathComponents + '/**/*.sass', ['sass']);
+	gulp.watch(pathSource + '/f/sass/**/*.sass', ['main-css']);
+	gulp.watch(pathComponents + '/**/i/*.*', ['img']);
+	gulp.watch(pathComponents + '/**/*.js', ['scripts']);
 });
 
 //default
-gulp.task('default', ['pug', 'sass', 'scripts', 'img', 'watch', 'browserSync'], function(){
+gulp.task('default', ['concat-pug-mixins', 'pug', 'main-css', 'sass', 'scripts', 'img', 'watch', 'browserSync'], function(){
 	console.log('gulp watch');
 });
 
-gulp.task('build',   ['pug', 'sass', 'scripts', 'img'], function(){
+gulp.task('build',   ['concat-pug-mixins', 'pug', 'main-css', 'sass', 'scripts', 'img'], function(){
 	console.log('build finished');
 });
